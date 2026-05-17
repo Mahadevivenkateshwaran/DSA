8b586f944576b08a510fdce2ad81a4f3c8903b42
Mahadevi <117967322+Mahadevivenkateshwaran@users.noreply.github.com>
2026-05-17 12:12:55 +0530

    Factorial

---
+++ src/Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5;
+        int result = calculateFactorial(num);
+        System.out.println(result);
+    }
+
+    private static int calculateFactorial(int num) {
+        if(num == 1) {
+            return 1;
+        }
+        return num * calculateFactorial(num -1);
+    }
+}
